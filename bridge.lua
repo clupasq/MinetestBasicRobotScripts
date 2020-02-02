e111baf822b044ae00c7bb5de273564a46999d31
wolf <cristian.lupascu@bolt.eu>
2020-02-02 11:42:12 +0200

    Refactor spiral into a self contained function

--- bridge.lua
+++ bridge.lua
@@ -1,12 +1,13 @@
 
-function bridge(pos, hradius, vradius)
+function bridge(pos, hradius, vradius, material)
+    material = material or { name = "default:wood" }
     midx = pos.x + hradius
     steps = 150
     for i = 1, steps, 1 do
         angle = i * (math.pi) / steps
         bx = math.floor(midx + math.cos(angle) * hradius)
         by = math.floor(pos.y + math.sin(angle) * vradius)
-        minetest.set_node({x = bx, y = by, z = pos.z}, { name = "default:wood" })
+        minetest.set_node({x = bx, y = by, z = pos.z}, material)
     end
 end
 
--- spiral.lua
+++ spiral.lua
@@ -1,17 +1,20 @@
-pos = { x = -140, y = 15,  z = -87 }
+function spiral(pos, radius, steps, material)
+    material = material or { name = "default:stone" }
+    for i = 1, steps, 1 do
+        angle = i * 0.1
+        elevation = i * 0.1
+        bx = math.floor(pos.x + math.cos(angle) * radius)
+        bz = math.floor(pos.z + math.sin(angle) * radius)
+        by = math.floor(pos.y + elevation)
+        minetest.set_node({x = bx, y = by + elevation, z = bz}, material)
+    end
+end
 
+pos = self.spawnpos()
 pos.x = pos.x + 2
 
-radius = 15
+spiral(pos, 15, 1000)
 
-for i = 1, 1000, 1 do
-  angle = i * 0.1
-  elevation = i * 0.1
-  bx = math.floor(pos.x + math.cos(angle) * radius)
-  bz = math.floor(pos.z + math.sin(angle) * radius)
-  by = math.floor(pos.y + elevation)
-  minetest.set_node({x = bx, y = by + elevation, z = bz}, { name = "default:stone" })
-end
 
 self.remove()
 
